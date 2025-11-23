ce24b6e068ab3e8d4bca815c263a326773d82876
izzyfgs <israelmalachy21@gmail.com>
2025-11-23 17:19:17 +0100

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,5 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
-    path('', include(router.urls)),  # include all router URLs
+    path('', include(router.urls)),
 ]
