d457548d8cec962d04c652579232954e1fa79b37
izzyfgs <israelmalachy21@gmail.com>
2025-11-23 17:15:29 +0100

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides full CRUD operations for Book model.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
