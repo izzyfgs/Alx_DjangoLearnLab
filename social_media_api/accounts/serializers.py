4fbfa5f0c6b0007ed15751ed40971d831d3a4ffe
izzyfgs <israelmalachy21@gmail.com>
2025-12-14 18:14:39 +0100

    Add social_media_api project with accounts app and serializers for auth

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,20 +5,21 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    # CharField for password
-    password = serializers.CharField(write_only=True)
+    username = serializers.CharField()
+    email = serializers.CharField()
+    password = serializers.CharField(write_only=True)  # must be CharField
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password')
 
     def create(self, validated_data):
-        # Create user using get_user_model().objects.create_user
+        # create user using get_user_model().objects.create_user
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
-        # Create a token for the new user
+        # create token for the user
         Token.objects.create(user=user)
         return user
