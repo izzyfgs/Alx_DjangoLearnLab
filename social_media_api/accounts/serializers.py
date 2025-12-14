2d13eb81ec7deb86e9acdce98fa933a20d0e67a2
izzyfgs <israelmalachy21@gmail.com>
2025-12-14 17:55:00 +0100

    Add social_media_api project with accounts app and serializers for auth

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,30 +1,22 @@
+
 from rest_framework import serializers
-from .models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'bio']
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
-            email=validated_data.get('email'),
-            password=validated_data['password'],
-            bio=validated_data.get('bio', '')
+            email=validated_data['email'],
+            password=validated_data['password']
         )
+        # create token for the new user
+        Token.objects.create(user=user)
         return user
-
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
-
-    def validate(self, data):
-        user = authenticate(**data)
-        if user:
-            return user
-        raise serializers.ValidationError("Invalid credentials")
